51102bcf221c33523f3f080f37b338de4aba165b
Anjy Gupta <anjyofficial@gmail.com>
2023-05-28 17:27:03 +0530

    fixed bugs and UI

--- app/schedule/Timeline.jsx
+++ app/schedule/Timeline.jsx
@@ -5,6 +5,7 @@ import Card from "./Card";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+
 export default function Timeline({ userArray, eventsArray, session }) {
   const eventCodes = [
     "IMPETUS",
@@ -24,19 +25,21 @@ export default function Timeline({ userArray, eventsArray, session }) {
           <div className="timeline-cont">
             <div className="timeline_header bg-transparent">
               <h1 className="about_h1">Registered Events By You</h1>
-              <div className="event_line"></div>
+              {/* <div className="event_line"></div> */}
             </div>
             {userArray.map((registered, index) => {
+              console.log(eventsArray)            
               if (registered === 1) {
+                
                 return (
                   <>
                     <div className="timeline-element" key={index}>
-                      <div className="rod">
+                      {/* <div className="rod">
                         <div className="outer_div">
                           <div className="inner_div"></div>
                         </div>
                         <div className="linetimeline"></div>
-                      </div>
+                      </div> */}
                       <Card
                         event={eventsArray[index]}
                         session={session}
@@ -58,6 +61,6 @@ export default function Timeline({ userArray, eventsArray, session }) {
   } else {
     return (
       <div className="flex justify-center mt-10">Please register something</div>
-    );
-  }
+);
+}
 }
